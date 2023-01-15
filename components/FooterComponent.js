299ebb5927dc4de5115e1b57caa92dd09dc4400f
David Oduneye <44040421+DOOduneye@users.noreply.github.com>
2023-01-15 13:32:02 -0800

    changed logo

--- components/FooterComponent.js
+++ components/FooterComponent.js
@@ -1,7 +1,9 @@
 const FooterComponent = () => {
     return (
         <footer className="absolute bottom-0 w-full flex flex-col items-center justify-center pb-5">
-            <p className="text-slate-100">Powered by <a href="https://www.themoviedb.org/" className="text-blue-300 hover:underline">The Movie Database</a></p>
+            <a href="https://www.themoviesdb.org/">
+                <img src="/tmdb-logo.svg" alt="The Movie Database" className="w-full" />
+            </a>
         </footer>
     )
 }
